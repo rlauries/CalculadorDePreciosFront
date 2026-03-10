abb5121b983808e33b591ce49f50ceaddcf55786
rlauries <rlauries001@gmail.com>
2026-03-10 09:53:43 -0400

    Changing things on Design Tailor component

--- src/componets/DesignTailorCard/DesignTailorCard.jsx
+++ src/componets/DesignTailorCard/DesignTailorCard.jsx
@@ -6,10 +6,10 @@ export const DesignTailorCard = () => {
   return (
     <AnimatedSection>      
       <span className='design-tailored-card'>
-          <img className='banner-image' src="images/20250430_1812_Arquitectura Moderna con Piscina_simple_compose_01jt4c18frez2v7ww98k0x6j55.png" alt="HeroBanner" />
+          <img className='banner-image' src="images/design-tailor-img.png" alt="HeroBanner" />
           
           <div className='second-banner-info'>
-              <h1>Designs Tailored to You</h1>
+              <h1>Designs Tailored to You <strong> |</strong></h1>
               <p>
                 Whether you're planning a custom pergola, fence, or a full backyard transformation — our expert team will create precise, professional architectural drawings tailored to your project. Save time, avoid design mistakes, and ensure your ideas meet local regulations.
             🛠️ We   turn your ideas into build-ready plans.
